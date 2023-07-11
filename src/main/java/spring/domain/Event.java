fa876a94580dc4b8763e43f9162716a470a3401e
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-07-11 10:20:18 +0200

    finish with classwork 3

---
+++ src/main/java/spring/domain/Event.java
@@ -0,0 +1,32 @@
+package spring.domain;
+
+public class Event {
+    private String name;
+    private String city;
+
+    public Event(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public Event() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+}
